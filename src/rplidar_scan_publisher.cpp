9a5bfcaf4987386ee8ae63c09e51be8c6798ab09
user <user@user.com>
2021-11-04 14:23:26 +0800

    clean

--- src/rplidar_scan_publisher.cpp
+++ src/rplidar_scan_publisher.cpp
@@ -1,37 +1,3 @@
-/*
- *  RPLIDAR ROS2 NODE
- *
- *  Copyright (c) 2009 - 2014 RoboPeak Team
- *  http://www.robopeak.com
- *  Copyright (c) 2014 - 2016 Shanghai Slamtec Co., Ltd.
- *  http://www.slamtec.com
- *
- */
- /*
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are met:
-  *
-  * 1. Redistributions of source code must retain the above copyright notice,
-  *    this list of conditions and the following disclaimer.
-  *
-  * 2. Redistributions in binary form must reproduce the above copyright notice,
-  *    this list of conditions and the following disclaimer in the documentation
-  *    and/or other materials provided with the distribution.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-  * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-  * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-  * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-  * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-  * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  */
-
 #include <signal.h>
 #include <spdlog/spdlog.h>
 #include <rclcpp/rclcpp.hpp>
@@ -39,19 +5,14 @@
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include "rplidar.h"
 
-
-#define DEG2RAD(x) ((x) * M_PI / 180.)
-#define ROS2VERSION "1.0.1"
-
 using namespace std;
 using namespace rp::standalone::rplidar;
-
 
 class RPLidarScanPublisher : public rclcpp::Node
 {
 public:
 	u_result ret = 0;
-	RPlidarDriver* drv = NULL;
+	rp::standalone::rplidar::RPlidarDriver* drv = NULL;
 	RPLidarScanPublisher() : Node("rplidar_scan_publisher") {}
 
 public:
@@ -66,6 +27,7 @@ public:
 	bool angle_compensate = this->declare_parameter("angle_compensate", true);
 	float max_distance = 8.0;
 	size_t npoint_per_degree = 1;//it stand of angle compensate at per 1 degree
+	int log_detail = this->declare_parameter("log_detail", 0); //1:RawScan   2:FormatScan   3:BothScan
 
 public:
 	rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr scan_pub = this->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::QoS(rclcpp::SensorDataQoS()));
@@ -131,7 +93,7 @@ public:
 	int work_loop(bool &exit)
 	{
 		//1.CreateDriver
-		RCLCPP_INFO(this->get_logger(), "ROS2 SDK Version:" ROS2VERSION ", RPLIDAR SDK Version:" RPLIDAR_SDK_VERSION "");
+		RCLCPP_INFO(this->get_logger(), "RPLidar_SDK_Version: %s\nRPLidar_ROS2_Version: 1.0.1 ", RPLIDAR_SDK_VERSION);
 		if (channel_type == "tcp") drv = RPlidarDriver::CreateDriver(rp::standalone::rplidar::DRIVER_TYPE_TCP);
 		else drv = RPlidarDriver::CreateDriver(rp::standalone::rplidar::DRIVER_TYPE_SERIALPORT);
 		if (!drv) { RCLCPP_ERROR(this->get_logger(), "Failed: create driver"); return -2; }
@@ -195,7 +157,7 @@ public:
 		}
 
 		//8.SpinROS
-		while (rclcpp::ok() && !exit)
+		for(int64_t frameId = 0; rclcpp::ok() && !exit; ++frameId)
 		{
 			rclcpp::spin_some(shared_from_this());
 
@@ -212,19 +174,22 @@ public:
 			if (IS_FAIL(ret)) { RCLCPP_ERROR(this->get_logger(), "Failed: ascendScanData and ret=%x", ret); continue; }
 
 			//8.3 RawScan
-			if (!angle_compensate)
+			if (angle_compensate == false || log_detail & 1)
 			{
 				int first_node = -1;
 				int final_node = meas_count;
 				while (meas_nodes[++first_node].dist_mm_q2 == 0);//First valid node
 				while (meas_nodes[--final_node].dist_mm_q2 == 0);//Final valid node
-				float angle_min = DEG2RAD(get_angle_deg(meas_nodes[first_node]));
-				float angle_max = DEG2RAD(get_angle_deg(meas_nodes[final_node]));
+				float angle_min = get_angle_deg(meas_nodes[first_node]) * M_PI / 180;
+				float angle_max = get_angle_deg(meas_nodes[final_node]) * M_PI / 180;
 				publish_scan(scan_pub, meas_nodes.data() + first_node, final_node - first_node + 1, scan_timestamp, frame_id, scan_duration, inverted, angle_min, angle_max, max_distance);
+				string scan_detail = fmt::format("RawScan{}: AngleMin={}, AngleMax={}, NodeCount={}", frameId, angle_min, angle_max, final_node - first_node + 1);
+				if (log_detail & 1) for (int k = first_node; k < final_node; ++k) scan_detail += fmt::format("\n\tNode{}: [ {}, {} ]", get_angle_deg(meas_nodes[k]), meas_nodes[k].dist_mm_q2 * 0.00025);
+				if (long(time(0)) % 2 == 0) RCLCPP_INFO(this->get_logger(), "%s", scan_detail.c_str());
 			}
 
 			//8.4 FormatScan
-			else
+			if (angle_compensate == true || log_detail & 2)
 			{
 				vector<rplidar_response_measurement_node_hq_t> formated_meas_nodes(360 * npoint_per_degree);
 				memset(formated_meas_nodes.data(), 0, formated_meas_nodes.size() * sizeof(formated_meas_nodes[0]));
@@ -242,6 +207,9 @@ public:
 						}
 					}
 				publish_scan(scan_pub, formated_meas_nodes.data(), formated_meas_nodes.size(), scan_timestamp, frame_id, scan_duration, inverted, 0.f, 359.f, max_distance);
+				string scan_detail = fmt::format("FormatScan{}: PointPerDegree={}, NodeCount={}", frameId, npoint_per_degree, formated_meas_nodes.size());
+				if (log_detail & 2) for (int k = 0; k < formated_meas_nodes.size(); ++k) scan_detail += fmt::format("\n\tNode{}: [ {}, {} ]", get_angle_deg(formated_meas_nodes[k]), formated_meas_nodes[k].dist_mm_q2 * 0.00025);
+				if (long(time(0)) % 2 == 0) RCLCPP_INFO(this->get_logger(), "%s", scan_detail.c_str());
 			}
 		}
 
